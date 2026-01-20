cb6f459c3950e64a2623d692c6268847314a784f
Oniro123 <konradman131@gmail.com>
2026-01-20 16:39:21 +0000

    Add database interaction

--- index.js
+++ index.js
@@ -1,59 +1,104 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "motivation_app",
+  password: "123",
+  port: 5432,
+});
+db.connect();
+
 let author_of_the_day = ""
 let quote_of_the_day = ""
-let quotes = []
-let authors = []
 
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+await fetchQuote();
+setInterval(fetchQuote, 3600 * 1000);
 
-app.get("/",async(req,res) =>{
-  try {
-    const response = await axios.get('https://zenquotes.io/api/random/');
-    author_of_the_day = response.data[0].a
-    quote_of_the_day = response.data[0].q
-    console.log(author_of_the_day)
-  } catch (error) {
-    console.error(error);
-  }
+app.get("/",(req,res) =>{
     res.render("index.ejs");
     
 });
 
 app.post('/comments', (req,res) =>{
-   res.render("comments.ejs",{
-    autor_del_dia: author_of_the_day,
-    cuotacion_del_dia: quote_of_the_day,
-    cuotaciones: quotes,
-    autores: authors,
+    let quotes = []
+    let authors = []
+    let ids = []
+    db.query("SELECT * FROM motivation_quotes", (err,result) => {
+      if (err){
+        console.error("Error executing query", err.stack);
+      }else{
+        for (let row of result.rows){
+          quotes.push(row.quote)
+          authors.push(row.author)
+        }
+           res.render("comments.ejs",{
+              autor_del_dia: author_of_the_day,
+              cuotacion_del_dia: quote_of_the_day,
+              cuotaciones: quotes,
+              autores: authors,
+              identificacion: ids,
   });
+      }
+      });
 });
 
 app.post('/comment_form', (req, res) => {
   const quoteNumber = parseInt(req.body.quote_number); 
   if (req.body.delete_post) {
-
-    quotes.splice(quoteNumber, 1);
-    authors.splice(quoteNumber, 1);
+      db.query("DELETE FROM motivation_quotes WHERE id = $1",[quoteNumber], (err, result)=>{
+        if (err){
+          console.error("Error executing query", err.stack);
+        }else{
+          console.log("Succesfully Deleted")
+        }
+      })
   } 
   else if (req.body.edit_post) {
     
     const newQuote = req.body.edit;
     if (newQuote && newQuote.trim() !== '') {
-      quotes[quoteNumber] = newQuote;
+
+        db.query("UPDATE motivation_quotes SET quote = $1 WHERE id = $2",[newQuote, quoteNumber], (err, result)=>{
+        if (err){
+          console.error("Error executing query", err.stack);
+        }else{
+          console.log("Succesfully Edited")
+        }
+      })
+      
     }
   }
   
-  res.render("comments.ejs", {
-    cuotaciones: quotes,
-    autores: authors,
+    let quotes = []
+    let authors = []
+    let ids = []
+    db.query("SELECT * FROM motivation_quotes", (err,result) => {
+      if (err){
+        console.error("Error executing query", err.stack);
+      }else{
+        for (let row of result.rows){
+          quotes.push(row.quote)
+          authors.push(row.author)
+          ids.push(row.id)
+        }
+           res.render("comments.ejs",{
+              autor_del_dia: author_of_the_day,
+              cuotacion_del_dia: quote_of_the_day,
+              cuotaciones: quotes,
+              autores: authors,
+              identificacion: ids,
   });
+      }
+      });
 });
 
 app.post('/post',(req,res)=>{
@@ -69,11 +114,28 @@ app.post('/',(req,res)=>{
       });
     }else{
       if (req.body.name == ""){
-        authors.push("Anonymous");
+
+        db.query("INSERT INTO motivation_quotes (author,quote) VALUES ('Anonymous', $1)", [req.body.quote], (err,res) => {
+          if (err){
+            console.error("Error executing query", err.stack);
+          }else{
+            console.log("Quote Uploaded Succesfully, it is Anonymous")
+          }
+          });
+
       }else{
-        authors.push(req.body.name);
+        console.log(req.body.name)
+        db.query("INSERT INTO motivation_quotes (author,quote) VALUES ($1, $2)",[req.body.name, req.body.quote], (err,res) => {
+          if (err){
+            console.error("Error executing query", err.stack);
+          }else{
+            console.log("Quote Uploaded Succesfully, it is NOT Anonymous")
+          }
+          });
+
       }
-    quotes.push(req.body.quote);
+
+
     res.render("index.ejs");
     }
 
@@ -89,3 +151,13 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+async function fetchQuote() {
+  try {
+    const response = await axios.get('https://zenquotes.io/api/random/');
+    author_of_the_day = response.data[0].a;
+    quote_of_the_day = response.data[0].q;
+    console.log("New quote fetched:", author_of_the_day);
+  } catch (err) {
+    console.error("Quote fetch failed:", err.message);
+  }
+}
